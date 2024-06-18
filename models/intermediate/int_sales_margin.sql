b0e6e5439f552d39e54175ad9b9d061c3a2de61b
Mohga Koshty <mohga.koshty@gmail.com>
2024-06-18 15:48:51 +0000

    remove shiping table

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,5 +4,3 @@ revenue-(quantity*CAST(purchase_price AS FLOAT64)) AS margin
 FROM {{ref("stg_raw__sales")}}
 LEFT JOIN {{ref("stg_raw__product")}}
 USING (products_id)
-LEFT JOIN {{ref("stg_raw__ship")}}
-USING (orders_id)
