8e5318ccd53545016f800f81efb5502d887dcc66
Mohga Koshty <mohga.koshty@gmail.com>
2024-06-19 14:17:55 +0000

    finance marts

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,4 +1,5 @@
-SELECT*
+SELECT*,
+operational_margin-ads_cost AS ads_margin
 FROM {{ref("finance_days")}}
-JOIN LEFT {{ref("int_campaigns_day")}}
+LEFT JOIN {{ref("int_campaigns_day")}}
 USING (date_date)
